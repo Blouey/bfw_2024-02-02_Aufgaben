8366d471814a96c01dcab7ba9ccc1c97a5912520
Blouey <philipklare@gmail.com>
2024-02-02 09:36:20 +0100

    init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _2024_02_02_WpfApp1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
